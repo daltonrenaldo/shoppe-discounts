0acebbab32383c0a3c09f6a18b1d2a821dd31722
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2016-07-16 15:44:16 -0400

    refactor, and ensure total_cost reflect discount

--- lib/shoppe/discounts/order_extensions.rb
+++ lib/shoppe/discounts/order_extensions.rb
@@ -8,26 +8,30 @@ module Shoppe
 
         # renamed total to total_before_discount
         alias_method :total_before_discount, :total
+        alias_method :total_cost_before_discount, :total_cost
         # total method is now the same as total_with_discount
         alias_method :total, :total_with_discount
+        alias_method :total_cost, :total_cost_with_discount
       end
 
       # new total method
       def total_with_discount
-        delivery_price +
-          delivery_tax_amount +
-          order_items.inject(BigDecimal(0)) { |t, i| t + i.total } -
-          discount_value
+        total_before_discount - discount_value(total_before_discount)
+      end
+
+      # new total_cost method
+      def total_cost_with_discount
+        total_cost_before_discount - discount_value(total_cost_before_discount)
       end
 
       private
 
-      def discount_value
+      def discount_value(original_cost)
         return BigDecimal(0) unless discount.present?
         if discount.unit == "cash"
           discount.value
         else
-          (discount.value * total_before_discount) / BigDecimal(100)
+          (discount.value * original_cost) / BigDecimal(100)
         end
       end
 
