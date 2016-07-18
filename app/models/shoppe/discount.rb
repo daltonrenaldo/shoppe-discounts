7530e4a9faea1452bab6286431899058b0f220ae
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2016-07-17 22:10:09 -0400

    discounts are active if there's no expiration date

--- app/models/shoppe/discount.rb
+++ app/models/shoppe/discount.rb
@@ -1,7 +1,7 @@
 module Shoppe
   class Discount < ActiveRecord::Base
     def active?
-      active && Time.current <= expire_at
+      active && (expire_at.nil? || Time.current <= expire_at)
     end
   end
 end
