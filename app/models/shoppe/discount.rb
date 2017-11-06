f9a0d78d83e3ca6375a931bfba6b169b369c1038
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2017-11-06 16:26:28 -0500

    helper methods

--- app/models/shoppe/discount.rb
+++ app/models/shoppe/discount.rb
@@ -3,5 +3,13 @@ module Shoppe
     def active?
       active && (expire_at.nil? || Time.current <= expire_at) && (max_uses == -1 || max_uses > use_count)
     end
+
+    def fixed?
+      unit == 'cash'
+    end
+
+    def percentage?
+      unit == 'percentage'
+    end
   end
 end
